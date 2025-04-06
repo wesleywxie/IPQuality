29bc2575aee58a8fc6d2dcf0a000f538c48c4797
Wesley Xie <wesley.w.xie@gmail.com>
2025-04-06 17:40:55 +0800

    fix: fixed format error

--- ip.sh
+++ ip.sh
@@ -348,7 +348,7 @@ esac
 # local RunTimes=$(curl ${CurlARG} -s --max-time 10 "https://hits.xykt.de/ip?action=hit" 2>&1)
 # stail[today]=$(echo "${RunTimes}"|jq '.daily')
 # stail[total]=$(echo "${RunTimes}"|jq '.total')
-}
+# }
 show_progress_bar(){
 local bar="\u280B\u2819\u2839\u2838\u283C\u2834\u2826\u2827\u2807\u280F"
 local n=${#bar}
